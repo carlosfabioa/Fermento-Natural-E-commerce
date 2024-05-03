f13a2283ed59a5b3194b56712382517ed1410b23
NeroWasTrolled <gabrielfrancasimoes@gmail.com>
2024-05-02 22:26:58 -0300

    Cristiano Ronaldo 02/05/2024

---
+++ appFermento-Natural-E-commerce/appFermento-Natural-E-commerce/Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace appFermento_Natural_E_commerce
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Adicione outros serviços aqui
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "Usuario",
+                    pattern: "{controller=Usuario}/{action=Index}/{id?}");
+            });
+        }
+
+    }
+}
---
+++ appFermento-Natural-E-commerce/appFermento-Natural-E-commerce/obj/Debug/net7.0/ec0zaead.sql
@@ -0,0 +1,31 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [usuario] (
+    [id] int NOT NULL IDENTITY,
+    [nome] nvarchar(max) NOT NULL,
+    [email] nvarchar(max) NOT NULL,
+    [senha] nvarchar(max) NOT NULL,
+    [endereço] nvarchar(max) NOT NULL,
+    [celular] int NOT NULL,
+    CONSTRAINT [PK_usuario] PRIMARY KEY ([id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240503000825_usuario', N'7.0.18');
+GO
+
+COMMIT;
+GO
+
